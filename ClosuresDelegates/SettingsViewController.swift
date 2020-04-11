2f042d65936ba5005ea45edf4ea0fba5a1d40023
Ed <esinyakov@baltbet.ru>
2020-04-11 13:09:40 +0300

    Initial Commit

---
+++ ClosuresDelegates/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  ClosuresDelegates
+//
+//  Created by Eduard Sinyakov on 4/11/20.
+//  Copyright © 2020 Eduard Siniakov. All rights reserved.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+	@IBAction private func viewBackgroundColorTapped(_ sender: UIButton) {
+	}
+
+	@IBAction private func buttonBackgroundColorTapped(_ sender: UIButton) {
+	}
+
+
+	@IBAction private func buttonTintColorTapped(_ sender: UIButton) {
+	}
+
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
